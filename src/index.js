b42d04a0d218a74aa4e58e39051671429912f332
adriano.souza <adriano.souza@zpesystems.com>
2022-04-11 18:43:28 -0300

    Creating deposit and withdraw

--- src/index.js
+++ src/index.js
@@ -20,6 +20,19 @@ function verifyExistsAccountCPF(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0)
+
+  return balance;
+
+}
+
 /**
  * Account fields:
  *  1) id - uuid
@@ -56,6 +69,46 @@ app.get("/statement", verifyExistsAccountCPF, (request, response) => {
   const { customer } = request;
 
   return response.json(customer.statement);
+});
+
+app.post("/deposit", verifyExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type:"credit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({error: "Insufficient funds!"});
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type:"debit"
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+
 });
 
 app.listen(3333);
